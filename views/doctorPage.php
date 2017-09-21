c49f5849a83e64f376469a3477116729156981f2
Dula <rdulmina@gmail.com>
2017-09-21 16:49:02 +0530

    doctor page finalized

--- views/doctorPage.php
+++ views/doctorPage.php
@@ -282,12 +282,6 @@
     <!-- /.container -->
 
     <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
-      </div>
-      <!-- /.container -->
-    </footer>
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
